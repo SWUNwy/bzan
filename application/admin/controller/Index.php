f53684b5ddf1188e1638acd3193096155bd6a58f
yu <swunwy@163.com>
2018-01-12 15:29:54 +0800

    update

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -2,7 +2,12 @@
 namespace app\admin\controller;
 use think\Controller;
 class Index extends Controller {
+    
     public function index() {
         return $this->fetch();
+    }
+
+    public function home() {
+    	return view();
     }
 }
