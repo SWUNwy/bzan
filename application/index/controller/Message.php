86b74b3994a5b49f5fe15d218779c2e73733ee17
wangyu <swunwy@163.com>
2018-01-19 23:11:43 +0800

    update

---
+++ application/index/controller/Message.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+/**
+ * MessageController
+ * 消息模块
+ * @author wyecho <[paul.wang@hotstaro2o.com]>
+ * @date(2018-1-19)
+ */
+class Message extends Controller {
+
+	public function index() {}
+}
---
+++ application/index/controller/User.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+/**
+ * UserController
+ * 用户模块
+ * @author wyecho <[paul.wang@hotstaro2o.com]>
+ * @date(2018-1-19)
+ */
+class User extends Controller {
+
+	public function index() {}
+}
