24074e3f735c2c3e3b8e1ab5a56b1b909c030e5f
yu <swunwy@163.com>
2018-04-12 18:06:38 +0800

    update

---
+++ application/wchat/controller/Common.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------
+// | bzan
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018~2022  All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: wyecho <@sina.com>
+// +----------------------------------------------------------------------
+namespace app\wchat\controller;
+
+use think\Controller;
+
+class Common extends Controller {
+
+	public function index() {
+		return $this->fetch();
+	}
+
+	public function tips() {
+		return $this->fetch();
+	}
+
+}
