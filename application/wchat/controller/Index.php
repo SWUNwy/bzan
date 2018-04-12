0d445df046d94f56af6d40be20107843b1da3f38
yu <swunwy@163.com>
2018-04-12 15:48:01 +0800

    add wchatModule

---
+++ application/wchat/controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | bzan
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018~2022  All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: wyecho <@sina.com>
+// +----------------------------------------------------------------------
+namespace app\wchat\controller;
+
+use think\Controller;
+
+class Index extends Controller {
+
+	public function index() {
+		return $this->fetch();
+	}
+
+}
