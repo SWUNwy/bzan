a35d2e22d26de7440ecde8642c9bee4f2b3a9ed5
yu <swunwy@163.com>
2018-04-13 17:21:31 +0800

    user register

--- application/index/controller/Register.php
+++ application/index/controller/Register.php
@@ -11,7 +11,12 @@
 namespace app\index\controller;
 
 use think\Controller;
-
+/**
+ * IndexController
+ * 商城用户注册模块
+ * @author wyecho <[paul.wang@hotstaro2o.com]>
+ * @date(2018-1-19)
+ */
 class Register extends Controller {
 
 	public function index() {
