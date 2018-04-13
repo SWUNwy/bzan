6d79e40af2c6403a5bb44389967969d5be8cabb2
yu <swunwy@163.com>
2018-04-13 17:11:57 +0800

    user login

---
+++ application/index/controller/Login.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------
+// | bzan
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018~2022  All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: wyecho <@sina.com>
+// +----------------------------------------------------------------------
+namespace app\index\controller;
+
+use think\Controller;
+/**
+ * IndexController
+ * 商城用户登录模块
+ * @author wyecho <[paul.wang@hotstaro2o.com]>
+ * @date(2018-1-19)
+ */
+class Login extends Controller {
+	
+    public function index() {
+        return $this->fetch();
+    }
+}
