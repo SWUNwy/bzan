e34e0ca5930338fe3b8ec2344b4a2bb15e5276ce
yu <swunwy@163.com>
2018-04-16 14:51:47 +0800

    add userModule

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -19,11 +19,28 @@ use think\Controller;
  */
 class User extends Controller {
 
+	/**
+	 * 用户中心首页
+	 * @return [type] [description]
+	 */
 	public function index() {
 		return $this->fetch();
 	}
 
+
+	/**
+	 * 用户个人信息页面
+	 * @return [type] [description]
+	 */
 	public function information() {
+		return $this->fetch();
+	}
+
+	/**
+	 * 用户安全设置页面
+	 * @return [type] [description]
+	 */
+	public function safety() {
 		return $this->fetch();
 	}
 
