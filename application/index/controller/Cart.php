8fd5a74dd81abf8785be301219c7e09a79c3c0e2
yu <swunwy@163.com>
2018-04-18 14:28:25 +0800

    add CartModule

--- application/index/controller/Cart.php
+++ application/index/controller/Cart.php
@@ -12,29 +12,16 @@ namespace app\index\controller;
 
 use think\Controller;
 /**
- * OrderController
- * 订单处理模块
+ * GoodsController
+ * 购物车模块控制器
  * @author wyecho <[paul.wang@hotstaro2o.com]>
  * @date(2018-1-19)
  */
-class Order extends Controller {
 
-    public function index() {}
+class Cart extends Controller {
 
-    /**
-     * 用户订单支付成功跳转页面
-     * @return [type] [description]
-     */
-    public function orderSuccess() {
-        return $this->fetch();
-    }
-    
-    /**
-     * 用户从购物车跳转到支付页面进行支付
-     * @return [type] [description]
-     */
-    public function pay() {
-    	return $this->fetch();
-    }
+	public function userCart() {
+		return $this->fetch();
+	}
 
 }
